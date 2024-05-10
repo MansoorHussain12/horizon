62fd640ba3dd05a54bf4499f3b7d17a6131fa0e9
Mansoor Hussain <qaisrani07@gmail.com>
2024-05-10 16:14:00 +0500

    Build AnimatedCounter, HeaderBox and TotalBalanceBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,25 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Homepage = () => {
-  return <div>homepage</div>;
+  const loggedIn = { firstName: "Mansoor" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account efficiently"
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Homepage;
