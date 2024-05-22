d43bd0cd7abc72151a0c22e54d0b73ff18c67a6d
Mansoor Hussain <qaisrani07@gmail.com>
2024-05-22 13:44:29 +0500

    Build my-banks page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,5 +1,37 @@
-const MyBanksPage = () => {
-  return <div>MyBanksPage</div>;
+import BankCard from "@/components/BankCard";
+import HeaderBox from "@/components/HeaderBox";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+const MyBanksPage = async () => {
+  const loggedInUser = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedInUser.$id,
+  });
+
+  return (
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox
+          title="My Bank Accounts"
+          subtext="Effortlessly manage your banking activities."
+        />
+        <div className="space-y-4">
+          <h2 className="header-2">Your Cards</h2>
+          <div className="flex flex-wrap gap-6">
+            {accounts &&
+              accounts.data.map((account: Account) => (
+                <BankCard
+                  key={account.id}
+                  account={account}
+                  userName={loggedInUser.firstName}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MyBanksPage;
